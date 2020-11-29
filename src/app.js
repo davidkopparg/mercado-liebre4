20ba641efe5cebec753b0ad794b57a9480aa69ac
davidkopparg <davidkopparg@gmail.com>
2020-11-29 16:47:42 -0300

    agregado login y register flexbox

--- src/app.js
+++ src/app.js
@@ -1,15 +1,20 @@
-const express = require("express");
+var express = require ("express");
 
-const app = express();
+var app= express();
+
+app.listen(3030,() => console.log(`Server running in ${3030} port`));
 
 
-app.listen(3030, function() {
-    console.log("servidor funcionando")
-});
-
-app.get ("/", function (req,res){
+app.get  ("/", function (req,res){
     res.sendFile(__dirname + "/views/index.html")
-    })
+})
+
+app.get  ("/register", function (req,res){
+    res.sendFile(__dirname + "/views/register.html")
+})
+app.get  ("/login", function (req,res){
+    res.sendFile(__dirname + "/views/login.html")
+})
 
 app.get('*', (req, res) => {
         res.sendFile(__dirname.replace("\src", "") + '/public/' + req.url);
